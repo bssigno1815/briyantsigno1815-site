8744690fcb833e49e6e28036e6d6af1c8af84bb4
bssigno1815 <briyantsigno@gmail.com>
2025-07-24 00:57:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
+      <h1>Byenvini sou sit Briyant Solèy Signo 1815</h1>
+      <p>Nou lanse platfòm ofisyèl nou pou tout fanatik ak manm.</p>
+    </div>
+  );
+}
